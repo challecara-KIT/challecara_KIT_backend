da05b5ae212a9894df6129eff22c641b4fc3e8c9
yamato0211 <kumayama0211@gmail.com>
2022-09-23 16:01:53 +0900

    [add] clotheテーブルの追加

--- db/model.py
+++ db/model.py
@@ -1,6 +1,6 @@
 from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy import Column
-from sqlalchemy.sql.sqltypes import String
+from sqlalchemy import Column, String, ForeignKey
+from sqlalchemy.orm import relationship
 from uuid import uuid4
 
 Base = declarative_base()
@@ -16,3 +16,14 @@ class User(Base):
     name = Column(String)
     email = Column(String, unique=True)
     password_hash = Column(String, nullable=True)
+    clothes = relationship("Clothe")
+
+
+class Clothe(Base):
+    __tablename__ = "clothe"
+    clothe_id = Column(String, primary_key=True, default=generate_uuid)
+    image_url = Column(String, nullable=False)
+    size = Column(String, nullable=False)
+    color = Column(String, nullable=False)
+    user_id = Column(String, ForeignKey("users.user_id"))
+    
