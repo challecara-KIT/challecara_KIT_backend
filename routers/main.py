05dbd68e770df924592c8d1b08ca7b81bd30eb0e
yamato0211 <kumayama0211@gmail.com>
2022-09-23 16:04:08 +0900

    [add] router設定

--- routers/main.py
+++ routers/main.py
@@ -1,5 +1,7 @@
 from fastapi import APIRouter
 from .users import user_router
+from .clothes import clothe_router
 
 router = APIRouter()
 router.include_router(user_router, prefix="/users", tags=["users"])
+router.include_router(clothe_router, prefix="/clothes", tags=["clothes"])
