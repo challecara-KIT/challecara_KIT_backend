8a40eb0a4090458cb9f35507a1cbb22384e85270
yamato0211 <kumayama0211@gmail.com>
2022-10-05 17:29:07 +0900

    [add] エラーハンドリングの追加

--- cruds/fashions.py
+++ cruds/fashions.py
@@ -1,7 +1,8 @@
 from fastapi import HTTPException
 from sqlalchemy.orm.session import Session
 from db.model import Fashion, Fashion_Clothes
-from schemas.fashions import FashionPayload,Fashion as FashionSchema
+from schemas.fashions import Fashion as FashionSchema
+from db.model import Clothe
 
 def create_fashion(db: Session, user_id: str, clothe_ids: list[str], name:str) -> FashionSchema:
     fashion_orm = Fashion(
@@ -12,6 +13,9 @@ def create_fashion(db: Session, user_id: str, clothe_ids: list[str], name:str) -
     db.commit()
     db.refresh(fashion_orm)
     for clothe_id in clothe_ids:
+        clothe = db.query(Clothe).filter(Clothe.clothe_id == clothe_id).first()
+        if clothe is None:
+            raise HTTPException(status_code=404, detail=f"{clothe_id} clothe not found")
         fashion = Fashion_Clothes(
             fashion_id=fashion_orm.fashion_id,
             clothe_id=clothe_id
